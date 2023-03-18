c1cb3374d300b1f3ae988eec887645486b0f6ce2
verrelio.cr <verrelioragna@gmail.com>
2023-03-18 20:02:45 +0700

    Initial Commit

---
+++ Social Network/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Social Network
+//
+//  Created by Verrelio C. Rizky on 18/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
