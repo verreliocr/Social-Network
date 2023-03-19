864c12b59caa6728a05e0fdd43708fc762dc7382
verrelio.cr <verrelioragna@gmail.com>
2023-03-19 10:07:23 +0700

    create models needed

---
+++ Social Network/Models/Post.swift
@@ -0,0 +1,15 @@
+//
+//  Post.swift
+//  Social Network
+//
+//  Created by Verrelio C. Rizky on 18/03/23.
+//
+
+import UIKit
+
+struct Post {
+    var id      : Int
+    var user    : User
+    var text    : String
+    var image   : UIImage
+}
---
+++ Social Network/Models/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  Social Network
+//
+//  Created by Verrelio C. Rizky on 18/03/23.
+//
+
+import Foundation
+
+struct User {
+    var id          : Int
+    var name        : String
+    var username    : String
+}
